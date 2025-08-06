d0a0933e5e4ed96bbee50078728f0154f54783ae
Francine Claire Punzalan <100173959+Francine-Claire@users.noreply.github.com>
2025-08-06 17:15:59 +0800

    Create password-generator.py

---
+++ password-generator.py
@@ -0,0 +1,78 @@
+import random
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to Francine's Password Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+# --------------------------------------------------------
+#
+# Easy Version - prints letters, symbols, and numbers in sequence.
+#
+# ran_letters = random.sample(letters, nr_letters)
+# ran_symbols = random.sample(symbols, nr_symbols)
+# ran_numbers = random.sample(numbers, nr_numbers)
+#
+# combination = ran_letters + ran_symbols + ran_numbers
+# print(f"[{', '.join(combination)}]")
+# print(f"Your password is: {''.join(combination)}")
+
+# -----------------------------------------------------------
+
+# Hard version - everything is at random position
+#
+# ran_letters = random.sample(letters, nr_letters)
+# ran_symbols = random.sample(symbols, nr_symbols)
+# ran_numbers = random.sample(numbers, nr_numbers)
+#
+# combination = ran_letters + ran_symbols + ran_numbers
+# total_combi = nr_letters + nr_symbols + nr_numbers
+#
+# ran_combi = random.sample(combination, total_combi)
+# print(f"[{', '.join(ran_combi)}]")
+# print(f"Your password is: {''.join(ran_combi)}")
+
+# --------------------------------------------------------
+
+# Angela's Version - I didn't use For loops
+# Easy Level
+
+# password = ""
+#
+# for char in range(0, nr_letters):
+#     password += random.choice(letters)
+#
+# for char in range(0, nr_symbols):
+#     password += random.choice(symbols)
+#
+# for char in range(0, nr_numbers):
+#     password += random.choice(numbers)
+#
+# print(f"Your password is: {password}")
+
+# --------------------------------------------------------
+#  Hard Level - Angela's Version
+
+password_list = []
+
+for char in range(0, nr_letters):
+    password_list.append(random.choice(letters))
+
+for char in range(0, nr_symbols):
+    password_list.append(random.choice(symbols))
+
+for char in range(0, nr_numbers):
+    password_list.append(random.choice(numbers))
+
+print(password_list)
+random.shuffle(password_list)
+print(password_list)
+
+password = ""
+for char in password_list:
+    password += char
+print(f"Your password is: {password}")
